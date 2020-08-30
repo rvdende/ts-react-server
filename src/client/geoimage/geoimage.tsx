bd15f9d8e7631ee9541132c53dbd8cd4b1cda6bf
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2020-08-30 17:37:52 +0200

    geo clean

--- src/client/geoimage/geoimage.tsx
+++ src/client/geoimage/geoimage.tsx
@@ -77,23 +77,6 @@ export class GeoImage extends React.Component {
     }
 }
 
-const HOOK_SVG = 'm129.03125 63.3125c0-34.914062-28.941406-63.3125-64.519531-63.3125-35.574219 0-64.511719 28.398438-64.511719 63.3125 0 29.488281 20.671875 54.246094 48.511719 61.261719v162.898437c0 53.222656 44.222656 96.527344 98.585937 96.527344h10.316406c54.363282 0 98.585938-43.304688 98.585938-96.527344v-95.640625c0-7.070312-4.640625-13.304687-11.414062-15.328125-6.769532-2.015625-14.082032.625-17.960938 6.535156l-42.328125 64.425782c-4.847656 7.390625-2.800781 17.3125 4.582031 22.167968 7.386719 4.832032 17.304688 2.792969 22.160156-4.585937l12.960938-19.71875v42.144531c0 35.582032-29.863281 64.527344-66.585938 64.527344h-10.316406c-36.714844 0-66.585937-28.945312-66.585937-64.527344v-162.898437c27.847656-7.015625 48.519531-31.773438 48.519531-61.261719zm-97.03125 0c0-17.265625 14.585938-31.3125 32.511719-31.3125 17.929687 0 32.511719 14.046875 32.511719 31.3125 0 17.261719-14.582032 31.3125-32.511719 31.3125-17.925781 0-32.511719-14.050781-32.511719-31.3125zm0 0'
-
-const HOOK_PATH = new Path2D(HOOK_SVG)
-const SCALE = .2
-const OFFSET = 80
-
-function draw(ctx: any, location: any) {
-    ctx.fillStyle = 'deepskyblue'
-    ctx.shadowColor = 'dodgerblue'
-    ctx.shadowBlur = 20
-    ctx.save()
-    ctx.scale(SCALE, SCALE)
-    ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET)
-    ctx.fill(HOOK_PATH)
-    ctx.restore()
-}
-
 function CanvasGeo(props: any) {
     const [locations, setLocations] = React.useState([])
     const canvasRef = React.useRef(null)
@@ -120,16 +103,9 @@ function CanvasGeo(props: any) {
         let imgdata = ctx.getImageData(0, 0, 800, 800);
         let processed = grayscale(imgdata);
         let processed2 = convolute(processed, [
-            -1,
-            0,
-            1,
-            -2,
-            0,
-            2,
-            -1,
-            0,
-            1
-        ], 1)
+            -1, 0, 1,
+            -2, 0, 2,
+            -1, 0, 1])
 
 
         ctx.putImageData(processed2, 0, 0);
@@ -153,7 +129,7 @@ function CanvasGeo(props: any) {
         return pixels;
     };
 
-    function convolute(pixels: ImageData, weights: any, opaque: any) {
+    function convolute(pixels: ImageData, weights: any) {
         var side = Math.round(Math.sqrt(weights.length));
         var halfSide = Math.floor(side / 2);
         var src = pixels.data;
@@ -170,7 +146,7 @@ function CanvasGeo(props: any) {
 
         var dst = output.data;
         // go through the destination image pixels
-        var alphaFac = opaque ? 1 : 0;
+        // var alphaFac = opaque ? 1 : 0;
         for (var y = 0; y < h; y++) {
             for (var x = 0; x < w; x++) {
                 var sy = y;
@@ -196,7 +172,7 @@ function CanvasGeo(props: any) {
                 dst[dstOff] = r;
                 dst[dstOff + 1] = g;
                 dst[dstOff + 2] = b;
-                dst[dstOff + 3] = a + alphaFac * (255 - a);
+                // dst[dstOff + 3] = a + alphaFac * (255 - a);
             }
         }
 
